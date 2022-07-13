b1730a9bb2635b92b4f005be6004de20ca9e61cc
Dan <safedevdve@proton.me>
2022-07-12 23:39:40 -0400

    added withdrawl functions

--- src/Escrow.sol
+++ src/Escrow.sol
@@ -1,20 +1,34 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.13;
 
+
+/**
+ * @dev This contract allows for a 'buyer' to lock funds with a 'seller' to recieve these funds after 
+ * the buyer determines conditions are met. At any time the seller may call off the deal and return 
+ * funds to the buyer. After a predetermined deadline is passed, the buyer may back out of the deal and
+ * reclaim their funds.
+ */
 contract Escrow {
 
   mapping(address => Agreement) public agreements;
 
   struct Agreement {
-    address seller;
+    address payable seller;
     uint256 amount;
-    uint64 timeLimit;
+    uint256 deadline;
     bool active;
   }
 
+  /**
+   * @dev Creates an Agreement struct indexed by msg.sender as the buyer. The buyer must send funds,
+   * record the recipient of these funds (the seller), and set a deadline for the seller to complete
+   * the terms of the agreement offchain.
+   *
+   * deadline should be a unix timestamp
+   */
   function startEscrow(
-    address seller,
-    uint64 timeLimit) 
+    address payable seller,
+    uint256 deadline) 
   public payable {
     require(msg.sender != seller, "Escrow: Addresses must be distinct");
     require(msg.value > 0, "Escrow: Payment not sent");
@@ -22,8 +36,44 @@ contract Escrow {
     
     agreements[msg.sender].seller = seller;
     agreements[msg.sender].amount = msg.value;
-    agreements[msg.sender].timeLimit = timeLimit;
+    agreements[msg.sender].deadline = deadline;
     agreements[msg.sender].active = true;
+  }
+
+  /**
+   * @dev When called by the buyer, this function sends the seller all funds and closes the agreement.
+   *
+   */
+  function releaseFunds() public {
+    require(agreements[msg.sender].active, "Escrow: No active agreement for this address");
+
+    agreements[msg.sender].seller.transfer(agreements[msg.sender].amount);
+    agreements[msg.sender].active = false;
+  }
+
+  /**
+   * @dev When called by the seller, this function returns funds to the buyer and closes the agreement.
+   *
+   */
+  function voidAgreement(address payable buyer) public {
+    require(agreements[buyer].active, "Escrow: No active agreement for this address");
+    require(msg.sender == agreements[buyer].seller, "Escrow: Only the buyer can void this agreement");
+
+    buyer.transfer(agreements[buyer].amount);
+    agreements[buyer].active = false;
+  }
+
+  /**
+   * @dev When called by the buyer after the deadline, this function sends the seller all funds and 
+   * closes the agreement.
+   *
+   */
+  function recallFunds() public {
+    require(agreements[msg.sender].active, "Escrow: No active agreement for this address");
+    require(block.timestamp >= agreements[msg.sender].deadline, "Escrow: Deadline not yet hit");
+
+    payable(msg.sender).transfer(agreements[msg.sender].amount);
+    agreements[msg.sender].active = false;
   }
 
   /****************** VIEW ******************/
@@ -40,8 +90,8 @@ contract Escrow {
     return agreements[buyer].amount;
   }
 
-  function getTimeLimit(address buyer) public view returns(uint64) {
-    return agreements[buyer].timeLimit;
+  function getDeadline(address buyer) public view returns(uint256) {
+    return agreements[buyer].deadline;
   }
  
 } 
--- test/Escrow.t.sol
+++ test/Escrow.t.sol
@@ -6,23 +6,160 @@ import "src/Escrow.sol";
 
 contract ContractTest is Test {
   Escrow escrow;
-  address addr0;
+  address payable addr0;
+
+  receive() external payable {}
 
   function setUp() public {
-    addr0 = 0x2DD0e7C4EfF32E33f5fcaeE2aadC99C691134bB5;
+    addr0 = payable(0x2DD0e7C4EfF32E33f5fcaeE2aadC99C691134bB5);
     escrow = new Escrow();
   }
 
   function testStartEscrowActive() public {
-    escrow.startEscrow{ value: 1 ether }(addr0, 1 days);
+    escrow.startEscrow{ value: 1 ether }(addr0, block.timestamp);
     assertTrue(escrow.isActive(address(this)));
   }
 
   function testStartEscrowValuesAccurate() public {
-    escrow.startEscrow{ value: 1 ether }(addr0, 1 days);
+    escrow.startEscrow{ value: 1 ether }(addr0, block.timestamp);
     
     assertEq(escrow.getSeller(address(this)), addr0);
     assertEq(escrow.getAmount(address(this)), 1 ether);
-    assertEq(escrow.getTimeLimit(address(this)), 1 days);
+    assertEq(escrow.getDeadline(address(this)), block.timestamp);
+  }
+
+  function testFailStartEscrowNondistinctAddress() public {
+    escrow.startEscrow{ value: 1 ether }(payable(this), block.timestamp);
+  }
+
+  function testFailStartEscrowNoPayment() public {
+    escrow.startEscrow(addr0, 1 days);
+  }
+
+  function testFailStartEscrowRepeatAddress() public {
+    escrow.startEscrow{ value: 1 ether }(addr0, block.timestamp);
+    escrow.startEscrow{ value: 1 ether }(addr0, block.timestamp);
+  }
+
+  function testReleaseFundsDeactivates() public {
+    escrow.startEscrow{ value: 1 ether }(addr0, block.timestamp);
+    escrow.releaseFunds();
+
+    assertFalse(escrow.isActive(address(this)));
+  }
+
+  function testReleaseFundsValueSent() public {
+    uint256 escrowValue = 1 ether;
+    escrow.startEscrow{ value: escrowValue }(addr0, block.timestamp);
+    uint256 balanceBefore = address(escrow).balance;
+
+    escrow.releaseFunds();
+    uint256 balanceAfter = address(escrow).balance; 
+
+    assertEq(balanceAfter, balanceBefore - escrowValue);
+  }
+
+  function testReleaseFundsValueRecieved() public {
+    uint256 escrowValue = 1 ether;
+    escrow.startEscrow{ value: escrowValue }(addr0, block.timestamp);
+    uint256 balanceBefore = addr0.balance;
+
+    escrow.releaseFunds();
+    uint256 balanceAfter = addr0.balance; 
+
+    assertEq(balanceAfter, balanceBefore + escrowValue);
+  }
+
+  function testFailReleaseFundsNotActive() public {
+    escrow.releaseFunds();
+  }
+
+  function testVoidAgreementDeactivates() public {
+    escrow.startEscrow{ value: 1 ether }(addr0, block.timestamp);
+
+    vm.prank(addr0);
+    escrow.voidAgreement(payable(this));
+
+    assertFalse(escrow.isActive(address(this)));
+  }
+
+  function testVoidAgreementValueSent() public {
+    uint256 escrowValue = 1 ether;
+    escrow.startEscrow{ value: escrowValue }(addr0, block.timestamp);
+    uint256 balanceBefore = address(escrow).balance;
+
+    vm.prank(addr0);
+    escrow.voidAgreement(payable(this));
+    uint256 balanceAfter = address(escrow).balance; 
+
+    assertEq(balanceAfter, balanceBefore - escrowValue);
+  }
+
+  function testVoidAgreementValueRecieved() public {
+    uint256 escrowValue = 1 ether;
+    escrow.startEscrow{ value: escrowValue }(addr0, block.timestamp);
+    uint256 balanceBefore = address(this).balance;
+
+    vm.prank(addr0);
+    escrow.voidAgreement(payable(this));
+    uint256 balanceAfter = address(this).balance; 
+
+    assertEq(balanceAfter, balanceBefore + escrowValue);
+  }
+
+  function testFailVoidAgreementNotActive() public {
+    vm.prank(addr0);
+    escrow.voidAgreement(payable(this));
+  }
+
+  function testFailVoidAgreementWrongAddress() public {
+    escrow.startEscrow{ value: 1 ether }(addr0, block.timestamp);
+    escrow.voidAgreement(payable(this));
+  }
+
+  function testRecallFundsDeactivates() public {
+    escrow.startEscrow{ value: 1 ether }(addr0, block.timestamp);
+    escrow.recallFunds();
+
+    assertFalse(escrow.isActive(address(this)));
+  }
+
+  function testRecallFundsValueSent() public {
+    uint256 escrowValue = 1 ether;
+    escrow.startEscrow{ value: escrowValue }(addr0, block.timestamp);
+    uint256 balanceBefore = address(escrow).balance;
+
+    escrow.recallFunds();
+    uint256 balanceAfter = address(escrow).balance; 
+
+    assertEq(balanceAfter, balanceBefore - escrowValue);
+  }
+
+  function testRecallFundsValueRecieved() public {
+    uint256 escrowValue = 1 ether;
+    escrow.startEscrow{ value: escrowValue }(addr0, block.timestamp);
+    uint256 balanceBefore = address(this).balance;
+
+    escrow.recallFunds();
+    uint256 balanceAfter = address(this).balance; 
+
+    assertEq(balanceAfter, balanceBefore + escrowValue);
+  }
+
+  function testFailRecallFundsNotActive() public {
+    escrow.recallFunds();
+  }
+
+  function testFailRecallFundsTooSoon() public {
+    escrow.startEscrow{ value: 1 ether }(addr0, block.timestamp + 1000);
+
+    escrow.recallFunds();
+  }
+
+  function testFailRecallFundsWrongAddress() public {
+    escrow.startEscrow{ value: 1 ether }(addr0, block.timestamp);
+
+    vm.prank(addr0);
+    escrow.recallFunds();
   }
 }
